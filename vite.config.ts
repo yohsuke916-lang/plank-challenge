aa133caf87bbb29ddc721c9706b1939fec5bd05a
Yosuke <yohsuke916@gmail.com>
2026-07-11 09:05:55 +0900

    Deploy PLANK to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? '/plank-challenge/' : '/',
   plugins: [
     react(),
     VitePWA({
